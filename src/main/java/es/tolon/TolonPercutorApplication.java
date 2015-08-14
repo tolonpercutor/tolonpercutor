cf1990c26aa00fcc71ddd1ae39c025cfc240d5a8
malvarez <manuel.alvarez.alvarez@cern.ch>
2015-08-14 15:57:09 +0200

    Initial version of the APP

---
+++ src/main/java/es/tolon/TolonPercutorApplication.java
@@ -0,0 +1,15 @@
+package es.tolon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application entry point.
+ */
+@SpringBootApplication
+public class TolonPercutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TolonPercutorApplication.class, args);
+    }
+}
